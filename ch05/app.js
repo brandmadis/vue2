5d0ca9c645e6ad47f1e9c29e079806e3aaf2e82d
brandmadis <brandmadis@gmail.com>
2018-04-13 15:38:06 +0000

    05-66

--- ch05/app.js
+++ ch05/app.js
@@ -27,8 +27,14 @@ var vm1 = new Vue({
     }
   }
 });
+
+vm1.newProp = 'New!';
+console.log(vm1)
+
+
 setTimeout(function(){
-  vm1.title='Changed by timer'
+  vm1.title='Changed by timer';
+  vm1.show();
 }, 3000);
 
 var vm2 = new Vue({
